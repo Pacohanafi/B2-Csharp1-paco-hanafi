c7d57b3def34db9b1eca1e995b9bed6abc411bb0
Casser <paco.hanafi@epsi.fr>
2020-10-05 14:40:48 +0200

    Question 2

---
+++ myApp/myApp/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myApp
+{
+    public class Commune
+    {
+        public string nom;
+        public int code_postale;
+        public int nb_habitant;
+    }
+}
--- myApp/myApp/Program.cs
+++ myApp/myApp/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+
 
 namespace myApp
 {
@@ -6,7 +10,65 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("re World!");
+
+            List<Commune> listeCommune = new List<Commune>();
+
+            while(true)
+            {
+                string choix_util = Menu_com();
+
+                if(choix_util == "1")
+                {
+                    Commune com = creerCommune();
+                    listeCommune.Add(com);
+                }
+            }
         }
+
+        public static string Menu_com()
+        {
+            string choix_util;
+            Console.WriteLine("1 Créer une nouvelle ville");
+            Console.WriteLine("2 Afficher l'ensemble des villes");
+            choix_util = Console.ReadLine();
+        
+            return choix_util;
+        }
+
+        public static Commune creerCommune()
+        {
+            Commune com = new Commune();
+            com.nom = demandeNom();
+            com.code_postale = demandeEntier("Quel est le code postale?");
+            com.nb_habitant = demandeEntier("Quel est le nombre d'habitant?");
+            return com;
+        }
+
+        public static string demandeNom()
+        {
+            Console.WriteLine("Nom de la commune?");
+            string nom = Console.ReadLine();
+            return nom;
+        }
+
+        public static int demandeEntier(string mess)
+        {
+            Console.WriteLine(mess);
+            string entier = Console.ReadLine();
+            int valeurConvert;
+
+            while (!int.TryParse(entier, out valeurConvert))
+            {
+                Console.WriteLine("Entrez un nombre valide svp");
+                entier = Console.ReadLine();
+            }
+
+            return valeurConvert;
+        }
+
     }
+
+
+
 }
